bd9ae3ebcdf3e4b60f11bab9b3af318fa5bf25e6
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2025-07-22 23:24:22 -0500

    Fix gruv

--- scrapers/gruv.py
+++ scrapers/gruv.py
@@ -13,7 +13,7 @@ class Gruv(Scraper):
         self.url = f'{self.base_url}/category/{category}'
         self.session = Session()
         self.session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
-        self.session.headers['Referer'] = 'https://www.gruv.com/category/4k_ultra_hd_bluray_movies_pre_order' # fair enough, it's a form
+        self.session.headers['Referer'] = f'https://www.gruv.com/category/4k_ultra_hd_bluray_movies_pre_order' # fair enough, it's a form
     
     def scrape(self):
         # get
@@ -34,15 +34,21 @@ class Gruv(Scraper):
                 continue
             title_tag = body_tag.select_one('a')
             price_tag = card.select_one('.price')
-            image_tag = card.select_one('.card-img-top')
+            image_tag = card.select_one('img')
             
-            if not price_tag or not title_tag or not image_tag:
-                print('Invalid card, missing price, title, or image tag', card)
+            if not price_tag:
+                print('Invalid card, missing price', card)
                 continue
-            price = float(price_tag.text.strip('$ '))
-            title = title_tag.text
+            if not title_tag:
+                print('Invalid card, missing title', card)
+                continue
+            if not image_tag:
+                print('Invalid card, missing image', card)
+                continue
+            price = float(price_tag.text.strip('$ \n'))
+            title = title_tag.text.strip()
             url = self.base_url + title_tag.attrs.get('href', '/') # /product/...
-            img = image_tag.attrs.get('src', '')
+            img = 'https' + image_tag.attrs.get('src', '').removeprefix('https:')
             found.append({
                 'price': price,
                 'title': title,
